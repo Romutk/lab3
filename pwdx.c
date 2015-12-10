9ddcf90d8f40618cc7dbe2a0e04c1ab28d741359
romanutkinhome <newangel88@mail.ru>
2015-12-10 21:29:22 +0300

    боримся за компиляцию

--- pwdx.c
+++ pwdx.c
@@ -1,3 +1,4 @@
+
 #include <errno.h>
 #include <getopt.h>
 #include <limits.h>
@@ -8,17 +9,6 @@
 #include <unistd.h>
 
 
-int check_pid_argument(char *input)
-{
-	int skip = 0;
-	long pid;
-	char *end = NULL;
-
-	pid = strtol(input + skip, &end, 10);
-
-	return 0;
-}
-
 int main(int argc, char *argv[])
 {
 	int ch;
@@ -26,25 +16,26 @@ int main(int argc, char *argv[])
 	int alloclen = 128;
 	char *pathbuf;
 
-	static const struct option longopts[] = {
-		{"version", no_argument, 0, 'V'},
-		{"help", no_argument, 0, 'h'},
-		{NULL, 0, 0, 0}
-	};
-
-
+m	while ((ch = getopt_long(argc, argv, "Vh", longopts, NULL)) != -1)
+		switch (ch) {
+		case 'V':
+			printf(PROCPS_NG_VERSION);
+			return EXIT_SUCCESS;
+		case 'h':
+			usage(stdout);
+		default:
+			usage(stderr);
+		}
 
 	argc -= optind;
 	argv += optind;
-
-	if (argc == 0)
-		usage(stderr);
 
 	pathbuf = malloc(alloclen);
 
 	for (i = 0; i < argc; i++) {
 		char *s;
 		ssize_t len, buflen;
+		/* Constant 10 is the length of strings "/proc/" + "/cwd" + 1 */
 		char *buf;
 		buflen = 10 + strlen(argv[i]) + 1;
 		buf = xmalloc(buflen);
@@ -54,24 +45,21 @@ int main(int argc, char *argv[])
 		else
 			snprintf(buf, buflen, "%s/cwd", argv[i]);
 
+		/*
+		 * buf contains /proc/NNNN/cwd symlink name
+		 * on entry, the target of that symlink on return
+		 */
 		while ((len = readlink(buf, pathbuf, alloclen)) == alloclen) {
 			alloclen *= 2;
 			pathbuf = realloc(pathbuf, alloclen);
 		}
+		free(buf);
 
-			 if (len < 0) {
-                	        s = strerror( errno);
-                       		 retval =1 ;
-                        fprintf(stderr, "%s: %s\n", argv[i], s);
-                        continue;
-                } else {
-                        pathbuf[len] = 0;
-                        s = pathbuf;
-                }
-
-                printf("%s: %s\n", argv[i], s);
-        }
-        free(pathbuf);
-        return retval;
+			pathbuf[len] = 0;
+			s = pathbuf;
+		
+		printf("%s: %s\n", argv[i], s);
+	}
+	free(pathbuf);
+	return retval;
 }
-
