777cfd820933d0fc2fcb1390830108d704324ab8
romanutkinhome <newangel88@mail.ru>
2015-12-10 23:11:02 +0300

    попытка используя функцию readlink выводить переменное окружкения по пиду который буду обохначать хххх

---
+++ pwdxmod.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[]) {
+	char *buf;
+	buf = malloc (128);
+	char *a;
+
+
+	a = readlink("/proc/xxxx/cwd" , buf , 128);
+	printf ("%s\n , a)
+	return 0;
+}
+
