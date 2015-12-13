d77462bd330522f74c2882c486390b9274bb2ce9
Roman <newangel88@mail.ru>
2015-12-13 17:07:56 +0300

    поправил синтаксис, компилится н все равно выдает ошибку при исполнении

--- pwdxmod.c
+++ pwdxmod.c
@@ -8,7 +8,7 @@ int main (int argc, char *argv[]) {
 	int a;
 	int c;
 
-for (c=1; c<argc;,c++) {
+for (c=1; c<argc;c++) {
 	sprintf(b,"proc/%s/cwd",argv[c]);
 	a = readlink(b,buf,PATH_MAX);
 	buf[a]=0;
