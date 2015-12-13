a62c7ee1162ecdc86a66d7b500c70e49a173ab50
Roman <newangel88@mail.ru>
2015-12-13 18:39:02 +0300

    zzzzz

--- pwdxmod.c
+++ pwdxmod.c
@@ -4,13 +4,18 @@
 #include <string.h>
 
 int main (int argc, char *argv[]) {
-	char buf[1024];
+//	char buf[1024];
 	char *b;
 	int a;
 	int c;
 
 for (c=1;c<argc;c++) {
-	sprintf(b,"/proc/%s/cwd",argv[c]);
+	buflen = 10 + strlen(argv[c])+1;
+	buf= xmalloc(buflen);
+//	sprintf(b,"/proc/%s/cwd",argv[c]);
+	sprintf (buf,buflen,"/proc/%S/cwd",argv[c]);
+
+
 	a = readlink(b,buf,PATH_MAX);
 	buf[a]=0;
 	printf ("%s:%s\n",argv[c],buf);
