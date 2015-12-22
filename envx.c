fd6f7beac41e3cc6ba8cc3d398d1c79e0331360f
Roman Utkin <newangel88@mail.ru>
2015-12-22 14:11:04 +0300

    изменил envx.c для работы с библиотекой mylibxxx

--- envx.c
+++ envx.c
@@ -1,36 +1,12 @@
 #include <errno.h>
-#include <getopt.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 #include <stdio.h>
+#include <unistd.h>
+#include <funxxx.h>
 
-	int main(int argc, char *argv[])
-	int i;
-	char c[1];
-	char *path[];
-	int alloclen = 128;
-
-argc -= optind;
-argv += optind;
-
-
-for (i = 0; i < argc; i++) {
-ssize_t  buflen;
-char *buf;
-buflen = 15 + strlen(argv[i]) + 1;
-path = malloc(buflen);
-
-snprintf(path, buflen, "/proc/%s/environ", argv[i]);
-
-
-	i=open(path,O_RDONLY);
-	read(i,c,1);
-	if (c!=|);
-	putc(c);
-else 
-putc('/n')
-
+int main (int argc, char* argv[])
+{
+char end_path=/environ
+int result = funxxx()
+printf ("%s\n", target_path);
+return 0;
+}
