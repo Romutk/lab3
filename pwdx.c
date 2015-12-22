0a274ff190d7e3980d8ac251d420841aa239046b
Roma <newangel88@mail.ru>
2015-12-23 00:38:54 +0400

    Добавил те же правки к pwdx.c

--- pwdx.c
+++ pwdx.c
@@ -1,12 +1,17 @@
-#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <funxxx.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mylibxxx.h"
 
-int main (int argc, char* argv[])
+int main(int argc, char const *argv[])
+//int main (int argc, char const * argv[])
 {
-char end_path=/cwd
-int result = funxxx()
-printf ("%s\n", target_path);
+if( argc<2 )  return 1;
+char output[256];
+char end_path="/cwd";
+//int result = funxxx();
+funxxx(argv[1],end_path, output, sizeof(output));
+printf ("%s\n", output);
 return 0;
 }
