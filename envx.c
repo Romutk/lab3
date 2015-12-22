4760c0e7388330103d70fb4e284d06f8de226695
Roma <newangel88@mail.ru>
2015-12-22 23:17:17 +0400

    Исправил передечу аргументов в функцию funxxx

--- envx.c
+++ envx.c
@@ -9,7 +9,8 @@ int main (int argc, char* argv[])
 {
 char output[256];
 char end_path="/environ";
-int result = funxxx();
+//int result = funxxx();
+funxxx(argv[1],end_path, output, sizeof(output));
 printf ("%s\n", output);
 return 0;
 }
