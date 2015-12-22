1de0ab99da436a1605844244aaa8cc7100d2825e
roma <roma@ubuntu-lab3.(none)>
2015-12-22 22:55:38 +0400

    убрал лишнее

--- mylibxxx.c
+++ mylibxxx.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <mylibxxx.h>
+#include "mylibxxx.h"
 
 
 char * funxxx (char *pid, char end_path, char *output, size_t limit) {
 	char str[80];
 
-// concatinate structure
 	strcpy(str, "/proc/");
 	strcat(str, pid);
 	strcat(str, end_path);
@@ -15,7 +14,7 @@ char * funxxx (char *pid, char end_path, char *output, size_t limit) {
 
 	printf("%s\n",str);
 
-// read from file
+
 	char ch;
 	FILE *fp;
 	fp = fopen(str,"r");
