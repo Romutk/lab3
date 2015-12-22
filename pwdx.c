2c643eb438daead398d901d418b445f7349ee789
Roman Utkin <newangel88@mail.ru>
2015-12-22 14:14:04 +0300

    изменил pwdx.c для работы с библиотекой mylibxxx

--- pwdx.c
+++ pwdx.c
@@ -1,42 +1,12 @@
 #include <errno.h>
-#include <getopt.h>
-#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <funxxx.h>
 
-int main(int argc, char *argv[])
+int main (int argc, char* argv[])
 {
-char ch;
-int retval = 0, i;
-int alloclen = 128;
-char *pathbuf;
-
-argc -= optind;
-argv += optind;
-
-pathbuf = malloc(alloclen);
-
-for (i = 0; i < argc; i++) {
-ssize_t len, buflen;
-char *buf;
-buflen = 10 + strlen(argv[i]) + 1;
-buf = malloc(buflen);
-
-snprintf(buf, buflen, "/proc/%s/cwd", argv[i]);
-
-while ((len = readlink(buf, pathbuf, alloclen)) == alloclen) {
-alloclen *= 2;
-pathbuf = realloc(pathbuf, alloclen);
-}
-free(buf);
-
-pathbuf[len] = 0;
-
-printf("%s: %s\n", argv[i], pathbuf);
-}
-free(pathbuf);
-return retval;
+char end_path=/cwd
+int result = funxxx()
+printf ("%s\n", target_path);
+return 0;
 }
