c16c6a6d0e18545ed188babb0c8e14d517f5af7e
Roma <newangel88@mail.ru>
2015-12-23 00:36:38 +0400

    добавил проверку

--- envx.c
+++ envx.c
@@ -4,9 +4,10 @@
 #include <string.h>
 #include "mylibxxx.h"
 
-
-int main (int argc, char* argv[])
+int main(int argc, char const *argv[])
+//int main (int argc, char const * argv[])
 {
+if( argc<2 )  return 1;
 char output[256];
 char end_path="/environ";
 //int result = funxxx();
